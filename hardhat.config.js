d6bb10ea4c5c112b928d7941e949cba3eea1ed28
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-06-09 00:52:39 +0100

    Config hardhat for deployment to rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+ const { INFURA_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.13",
+  solidity: "0.8.9",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: INFURA_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
